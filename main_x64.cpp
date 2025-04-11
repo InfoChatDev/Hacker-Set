cee3e12e47afedf2c7a74163b80a92efbed144e8
opzc35 <opzc35@outlook.com>
2025-04-11 15:29:15 +0800

    Create main_x64.cpp

---
+++ main_x64.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  system("Disk.exe");
+  system("Memory.exe");
+  system("Network.exe");
+  system("CPU.exe");
+  system("GPU.exe");
+  system("main.exe");
+}
+//Main
